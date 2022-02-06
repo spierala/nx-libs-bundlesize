7a3bded16b1122a750e4cb76330d7dd3e8682053
Florian Spier <mail@florian-spier.de>
2022-02-06 10:13:29 +0100

    nx g @nrwl/workspace:remove my-lib

--- libs/my-lib/src/lib/my-lib.spec.ts
+++
@@ -1,7 +0,0 @@
-import { myLib } from './my-lib';
-
-describe('myLib', () => {
-  it('should work', () => {
-    expect(myLib()).toEqual('my-lib');
-  });
-});
--- libs/my-lib/src/lib/my-lib.ts
+++
@@ -1,3 +0,0 @@
-export function myLib(): string {
-  return 'my-lib';
-}
